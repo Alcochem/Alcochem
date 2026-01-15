1cc5914088c9535d29af1f31895057ae5cc053a7
Niels-dooit <niels@dooit.nl>
2026-01-15 13:47:56 +0100

    V16

--- sustainability/models/carbon_factor_contributor.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class CarbonFactorContributor(models.Model):
-    _name = "carbon.factor.contributor"
-    _description = "Carbon Factor Contributor"
-    _inherit = ["mail.thread", "mail.activity.mixin", "carbon.copy.mixin"]
-
-    name = fields.Char(required=True)
